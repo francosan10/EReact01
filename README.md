169b7b8c4789db1aeea1f21f2787787ce60e8d41
Franco <lmassa.fms@gmail.com>
2024-02-05 14:23:52 -0300

    limpiamos el template y agregamos bootstrap

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio 1
+
+Crear una aplicación web con un componente que muestre en pantalla el mensaje “Hello World”.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center my-4">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
